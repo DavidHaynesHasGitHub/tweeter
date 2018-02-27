0dcc2c1f6d91ea115b206f7dfb5c237d2a5a6ffd
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-27 12:27:08 -0800

    added the count down functionality to the character counter. moving on to posting tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+const maxLetters = 140;
+
+$(document).ready(function () {
+    console.log('ready');
+
+    $('.new-tweet textarea').on('keyup', function () {
+        let lettersUsed = $(this).val().length;
+
+        $(this).parent().children('.counter').text(140 - lettersUsed);
+
+        if (lettersUsed > maxLetters) {
+            $(this).parent().children('.counter').css('color', 'red');
+        } 
+    });
+})
