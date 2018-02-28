6543de734ffa08d1954b2ae4c43d97f898a8dc66
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-28 15:56:47 -0800

    went through a code review with Joel. some minor formatting things that were corrected for ease of use. cleaned up code and added more comments for future reference

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(".new-tweet form").on('submit', function (e) {
 
   let tweetBody = $('#newTweet textarea[name="tweetText"]').val();
 
-  if (!tweetBody) {
+  if (!tweetBody.trim.length) {
    alert("You cant tweet nothing!");
    return;
   };
@@ -19,6 +19,7 @@ $("#nav-bar .compose").on('click', function (event) {
      }
   });
 })
+
 //prevents malicious code from being inserted with the user tweet
 function escape(str) {
   var div = document.createElement('div');
@@ -27,38 +28,44 @@ function escape(str) {
 };
 
 //stages the tweets to be rendered after clearing the
-let loadTweets = function () {
-  $.get("/tweets", function (data) {
-    $("tweetFeed").remove();
-      renderTweets(data);
-  });
+const loadTweets = function () {
+  $.get("/tweets", renderTweets);
 };
 
 //adds tweetText to /tweets to be rendered by loadTweets function
-let postNewTweet = function (tweetBody) {
+const postNewTweet = function (tweetBody) {
   let tweetText = {
       text: tweetBody
   };
 
-  $.post("/tweets", tweetText, function (data) {
-    loadTweets();
-  });
+  $.post("/tweets", tweetText, loadTweets, animate)
 
   //clear the form after submit
   $(".new-tweet form textarea").val("");
 };
 
 // RENDERS THE TWEET OBJECTS
-let renderTweets = function (data) {
+const renderTweets = function (data) {
+  var $tweetFeed = $(".tweetFeed").empty();
   for (let tweet of data) {
     let $tweetElement = createTweetElement(tweet);
-    $(".tweetFeed").prepend($tweetElement);
+    animate($tweetElement)
+    $tweetFeed.prepend($tweetElement);
   }
 };
+
+//adds animations to all tweets on hover
+const animate = function () {
+  $(".article").hover(function () {
+    $("#footer-icons").toggleClass("animated bounce");
+    $(this).toggleClass("animated bounce");
+  })
+};
+
 //ALL FUNCTIONS REGARDING BUILDING THE TWEET OBJECT
 
 //creates the header element for the tweet
-let createHeader = function (tweet){
+const createHeader = function (tweet){
   let $header = $('<header>').addClass('tweet-header');
   let $img = $('<img src=' + tweet.user.avatars.small + '>');
   let $h2 = $('<h2>' + tweet.user.name + '</h2>');
@@ -68,7 +75,7 @@ let createHeader = function (tweet){
   return $header;
 };
 //creates the footer element for the tweet
-let createFooter = function (tweet){
+const createFooter = function (tweet){
   let timePosted = tweet.created_at / 1000;
   let $footer = $('<footer>').addClass('tweet-footer')
   let $p = $('<p data-livestamp=' + timePosted + '></p>')
@@ -77,21 +84,18 @@ let createFooter = function (tweet){
               .append('<i class="material-icons">sync</i>')
               .append('<i class="material-icons">favorite</i>')
 
-  $footer.append($p).append($footerIcons)
+  $footer.append($p).append($footerIcons).append('<div class="clearfix">')
   return $footer;
 };
 //creates the main article elements for the tweet
-let createTweetElement = function (tweet) {
-  let $tweetArticle = $(`<section class='tweets'>`);
-  let $innerArticle = $("<article>");
+const createTweetElement = function (tweet) {
+  let $tweet = $("<article>");
 
   let $header = createHeader(tweet);
-  $innerArticle.append($header).append("<div class='tweet-body clearfix'><p>" + tweet.content.text);
+  $tweet.append($header).append("<div class='tweet-body clearfix'><p>" + tweet.content.text);
 
-  let $footer = createFooter(tweet);
-  $innerArticle.append($footer);
-  $tweetArticle.append($innerArticle);
+  let $footer = createFooter(tweet).appendTo($tweet);
 
-  return $tweetArticle;
+  return $tweet;
 };
 loadTweets();
